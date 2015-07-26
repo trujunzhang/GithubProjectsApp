67a81a477c053184bceaf862c2372fbc3cbece9f
Trujun Zhang <trujunzhang@gmail.com>
2015-07-26 11:13:23 +0800

    xxx

--- app/src/main/java/com/restaurant/tracking/githubprojectsapp/DateTimePickerActivity.java
+++ app/src/main/java/com/restaurant/tracking/githubprojectsapp/DateTimePickerActivity.java
@@ -7,7 +7,9 @@ import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * Created by djzhang on 7/26/15.
@@ -24,12 +26,19 @@ public class DateTimePickerActivity extends AppCompatActivity {
 
         this.setContentView(R.layout.activity_datepicker);
 
+
+        getNextHourDate();
+
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 DateTimePickerDialog dateTimePickerDialog = new DateTimePickerDialog(context, new DateTimePickerDialog.OnDateTimeSetListener() {
                     @Override
                     public void onDateTimeSet(DatePicker datePickerView, TimePicker timePickerView, int year, int monthOfYear, int dayOfMonth, int hourOfDay, int minute) {
+                        GregorianCalendar calendar = new GregorianCalendar(year,monthOfYear,dayOfMonth,hourOfDay,minute);
+                        Date endDate = calendar.getTime();
+
+
 
                     }
                 }, new Date(), true);
@@ -37,5 +46,12 @@ public class DateTimePickerActivity extends AppCompatActivity {
                 dateTimePickerDialog.show();
             }
         });
+    }
+
+    private void getNextHourDate() {
+        Calendar c = Calendar.getInstance();
+        c.setTime(new Date());
+        c.add(Calendar.HOUR, 1);
+        Date d = c.getTime();
     }
 }
