7d790789bd1ee4069cf7f282aac4ebdc000d054d
trujun zhang <trujunzhang@gmail.com>
2015-07-29 10:22:31 +0800

    xxx

--- app/src/main/java/com/restaurant/tracking/githubprojectsapp/SmartLocationActivity.java
+++ app/src/main/java/com/restaurant/tracking/githubprojectsapp/SmartLocationActivity.java
@@ -1,10 +1,16 @@
 package com.restaurant.tracking.githubprojectsapp;
 
 import android.content.Context;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.restaurant.tracking.githubprojectsapp.utils.LocationObserver;
+
+import io.nlopez.smartlocation.SmartLocation;
 
 /**
  * Created by djzhang on 7/29/15.
@@ -12,11 +18,38 @@ import com.restaurant.tracking.githubprojectsapp.utils.LocationObserver;
 public class SmartLocationActivity extends LocationObserver {
 
 
+    private TextView locationText;
+    private Button button2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        this.startLocation();
+        setContentView(R.layout.activity_smart_location);
+
+        locationText = (TextView) findViewById(R.id.location_text);
+        button2 = (Button) findViewById(R.id.button2);
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showLast();
+            }
+        });
+
+    }
+
+
+    private void showLast() {
+        Location lastLocation = SmartLocation.with(this).location().getLastLocation();
+        if (lastLocation != null) {
+            locationText.setText(
+                    String.format("[From Cache] Latitude %.6f, Longitude %.6f",
+                            lastLocation.getLatitude(),
+                            lastLocation.getLongitude())
+            );
+        }
 
     }
 }
