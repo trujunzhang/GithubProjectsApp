6ba7f14eed835d5e72043a3cf483cf2406922dc4
trujun zhang <trujunzhang@gmail.com>
2015-08-05 16:56:09 +0800

    EventBus demo.

--- app/src/main/java/com/restaurant/tracking/githubprojectsapp/eventbus/PageOneActivity.java
+++ app/src/main/java/com/restaurant/tracking/githubprojectsapp/eventbus/PageOneActivity.java
@@ -16,21 +16,9 @@ import de.greenrobot.event.Subscribe;
 /**
  * Created by djzhang on 8/5/15.
  */
-public class PageOneActivity extends AppCompatActivity {
+public class PageOneActivity extends TaskActivity {
 
     private TextView textView3;
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        try {
-            EventBus.getDefault().unregister(this);
-        } catch (Throwable t) {
-            //this may crash if registration did not go through. just be safe
-        }
-
-    }
 
 
     @Override
@@ -46,9 +34,6 @@ public class PageOneActivity extends AppCompatActivity {
                 showPageTwo();
             }
         });
-
-        // Register
-        EventBus.getDefault().register(this);
     }
 
     private void showPageTwo() {
--- app/src/main/java/com/restaurant/tracking/githubprojectsapp/eventbus/TaskActivity.java
+++ app/src/main/java/com/restaurant/tracking/githubprojectsapp/eventbus/TaskActivity.java
@@ -1,35 +1,34 @@
 package com.restaurant.tracking.githubprojectsapp.eventbus;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import de.greenrobot.event.EventBus;
 
 /**
  * Created by djzhang on 8/5/15.
  */
 public class TaskActivity extends AppCompatActivity {
 
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//        EventBus.getDefault().register(this);
-//    }
-//
-//    @Override
-//    protected void onDestroy() {
-//        super.onDestroy();
-//
-//        try {
-//            EventBus.getDefault().unregister(this);
-//        } catch (Throwable t) {
-//            //this may crash if registration did not go through. just be safe
-//        }
-//
-//    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-//    @SuppressWarnings("UnusedDeclaration")
-//    public void onEventMainThread(PostedModelEvent ignored) {
-//        //we could just add this to top or replace element instead of refreshing whole list
-//        EventPageInfo model = ignored.getModel();
-//        String displayName = model.displayName;
-//    }
+        // Register
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        try {
+            EventBus.getDefault().unregister(this);
+        } catch (Throwable t) {
+            //this may crash if registration did not go through. just be safe
+        }
+
+    }
+
+
 }
