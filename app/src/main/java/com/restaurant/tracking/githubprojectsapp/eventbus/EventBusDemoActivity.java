36ae15244c20301ea000b982ca426eb09582d044
trujun zhang <trujunzhang@gmail.com>
2015-08-05 15:36:04 +0800

    EventBus demo.

--- app/src/main/java/com/restaurant/tracking/githubprojectsapp/eventbus/EventBusDemoActivity.java
+++ app/src/main/java/com/restaurant/tracking/githubprojectsapp/eventbus/EventBusDemoActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import com.restaurant.tracking.githubprojectsapp.R;
 
 import de.greenrobot.event.EventBus;
+import de.greenrobot.event.Subscribe;
 
 /**
  * Created by djzhang on 8/5/15.
@@ -16,12 +17,17 @@ public class EventBusDemoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_bus_demo);
-        EventBus.getDefault().register(this);
+//        EventBus.getDefault().register(this);
     }
 
     @Override
     protected void onDestroy() {
-        EventBus.getDefault().unregister(this);
+//        EventBus.getDefault().unregister(this);
         super.onDestroy();
+    }
+
+    @Subscribe
+    public void xxx(){
+
     }
 }
