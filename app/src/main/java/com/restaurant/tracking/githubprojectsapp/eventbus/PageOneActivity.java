0f6ea82208deb2777a21b8c7a1c174f9ba8b9ad5
trujun zhang <trujunzhang@gmail.com>
2015-08-05 14:11:33 +0800

    EventBus demo.

--- app/src/main/java/com/restaurant/tracking/githubprojectsapp/eventbus/PageOneActivity.java
+++ app/src/main/java/com/restaurant/tracking/githubprojectsapp/eventbus/PageOneActivity.java
@@ -1,7 +1,35 @@
 package com.restaurant.tracking.githubprojectsapp.eventbus;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.restaurant.tracking.githubprojectsapp.R;
+
 /**
  * Created by djzhang on 8/5/15.
  */
-public class PageOneActivity extends TaskActivity{
+public class PageOneActivity extends TaskActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.setContentView(R.layout.activity_pageone);
+
+        this.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPageTwo();
+            }
+        });
+    }
+
+    private void showPageTwo() {
+        Intent intent = new Intent(this, PageTwoActivity.class);
+
+        this.startActivity(intent);
+    }
+
+
 }
--- app/src/main/java/com/restaurant/tracking/githubprojectsapp/eventbus/PageTwoActivity.java
+++ app/src/main/java/com/restaurant/tracking/githubprojectsapp/eventbus/PageTwoActivity.java
@@ -1,7 +1,16 @@
 package com.restaurant.tracking.githubprojectsapp.eventbus;
 
+import android.os.Bundle;
+
 /**
  * Created by djzhang on 8/5/15.
  */
-public class PageTwoActivity extends TaskActivity{
+public class PageTwoActivity extends TaskActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
 }
